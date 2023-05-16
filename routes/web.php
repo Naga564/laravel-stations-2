ecf67d40ac6c80109674c308868a4bc6ab2176dd
Naga564 <yanagayakisoba@gmail.com>
2023-05-16 18:28:37 +0900

    "railway 2 complete"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,20 +13,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/practice', [App\Http\Controllers\PracticeController::class, 'sample']);
+Route::get('/practice2', [App\Http\Controllers\PracticeController::class, 'sample2']);
+Route::get('/practice3', [App\Http\Controllers\PracticeController::class, 'sample3']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 //ルーティング
-Route::get('practice', function() {
-    return response('practice');
-});
-Route::get('practice2', function() {
-    $test = 'practice2';
-return response($test);
-});
-Route::get('practice3', function() {
-    $message="test";
-return response($message);
-});
+// Route::get('practice', function() {
+//     return response('practice');
+// });
+// Route::get('practice2', function() {
+//     $test = 'practice2';
+// return response($test);
+// });
+// Route::get('practice3', function() {
+//     $message="test";
+// return response($message);
+// });
